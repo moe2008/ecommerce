802eb23cbe29c91a35ccbf515674c49d7d2a8981
moe2008 <moritzdierker@gmx.de>
2024-09-19 15:41:36 +0200

    .

--- helpers/analyticsHelper.js
+++ helpers/analyticsHelper.js
@@ -1,4 +1,5 @@
 export const sendEvent = (action, category, label, value) => {
+    console.log("Sending event:", { action, category, label, value });
     window.gtag('event', action, {
       event_category: category,
       event_label: label,
