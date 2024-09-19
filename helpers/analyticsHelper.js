11716a168b0b36bf754a04c71570ca8566ae78ac
moe2008 <moritzdierker@gmx.de>
2024-09-19 16:00:37 +0200

    eventid

--- helpers/analyticsHelper.js
+++ helpers/analyticsHelper.js
@@ -1,8 +1,10 @@
 export const sendEvent = (action, category, label, value) => {
     console.log("Sending event:", { action, category, label, value });
+    const eventId = Math.random().toString(36).substring(7);
     window.gtag('event', action, {
       event_category: category,
       event_label: label,
       value: value,
+      event_id: eventId,
     });
   };
